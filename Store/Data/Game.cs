2f0040723e1112ce8949577c8ac9b2a4c8fbb963
Holker <haron.dev@gmail.com>
2018-05-23 20:38:43 +0200

    Added project

---
+++ Store/Data/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Store.Data
+{
+    class Game
+    {
+        string gameName = "";
+        float price = 0.0f;
+        string genre = "";
+        DateTime date = DateTime.Today;
+        string publisher = "";
+        float grade = 0.0f;
+        string description = "";
+        
+        [XmlElement("Game-name")]
+        public string GameName
+        {
+            get { return gameName; }
+            set { gameName = value; }
+        }
+        [XmlElement("Price")]
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        [XmlElement("Genre")]
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+        [XmlElement("Date")]
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        [XmlElement("Publisher")]
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+        [XmlElement("Grade")]
+        public float Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        [XmlElement("Description")]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int GameId
+        {
+            get;
+            internal set;
+        }
+
+    }
+}
---
+++ Store/Forms/GameForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Store.Forms
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
