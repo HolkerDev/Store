63d31549dac55aea1b889399a4525517fbb4d0d2
Holker <haron.dev@gmail.com>
2018-05-24 12:32:58 +0200

    Album Serialized

--- Store/Serialized/AlbumXmlSerialized.cs
+++ Store/Serialized/AlbumXmlSerialized.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+using Store.Data;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,20 @@ namespace Store.Serialized
 {
     class AlbumXmlSerialized
     {
+        public static void Serialize(Album client, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Album));
+            TextWriter textWriter = new StreamWriter(filePath);
+            serializer.Serialize(textWriter, client);
+            textWriter.Close();
+        }
 
+        public static Album Deserialize(string filePath)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Album));
+            TextReader textReader = new StreamReader(filePath);
+            Album client = (Album)deserializer.Deserialize(textReader);
+            return client;
+        }
     }
 }
