5b9ce85c2a3f62e332c3b2e2bcadcd0838da4cf9
Holker <haron.dev@gmail.com>
2018-05-24 11:47:45 +0200

    Added Movie class

---
+++ Store/Data/Movie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data
+{
+    class Movie
+    {
+        string movieName = "";
+        float price = 0.0f;
+        string genre = "";
+        DateTime date = DateTime.Today;
+        string author = "";
+        float grade = 0.0f;
+        string description = "";
+
+        [XmlElement("Movie-name")]
+        public string GameName
+        {
+            get { return movieName; }
+            set { movieName = value; }
+        }
+        [XmlElement("Price")]
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        [XmlElement("Genre")]
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+        [XmlElement("Date")]
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        [XmlElement("Author")]
+        public string Publisher
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        [XmlElement("Grade")]
+        public float Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        [XmlElement("Description")]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int MovieId
+        {
+            get;
+            internal set;
+        }
+    }
+}
