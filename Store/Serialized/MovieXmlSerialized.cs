ada109e52100f5ff13543dccaaa211fd69d2aad8
Holker <haron.dev@gmail.com>
2018-05-24 12:31:35 +0200

    Movie Serialized

--- Store/Serialized/MovieXmlSerialized.cs
+++ Store/Serialized/MovieXmlSerialized.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Store.Data;
+using System.IO;
+using System.Xml.Serialization;
 using System.Threading.Tasks;
 
 namespace Store.Serialized
 {
     class MovieXmlSerialized
     {
+        public static void Serialize(Movie client, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Movie));
+            TextWriter textWriter = new StreamWriter(filePath);
+            serializer.Serialize(textWriter, client);
+            textWriter.Close();
+        }
+
+        public static Movie Deserialize(string filePath)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Movie));
+            TextReader textReader = new StreamReader(filePath);
+            Movie client = (Movie)deserializer.Deserialize(textReader);
+            return client;
+        }
     }
 }
